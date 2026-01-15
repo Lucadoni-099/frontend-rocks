12528f7eda8649556b98c4600a2edb266eb18749
LUCA <lucadoniselli07@gmail.com>
2026-01-15 13:22:48 +0000

    PRIMA VERSIONE

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -1,3 +1,14 @@
+const stilecard="bg-purple-500 w-40 h-40 text-center flex items-center justify-center";
+
+
 export const Detail = () => {
-  return null;
+
+  return <div className=" text-5xl space-x-7 flex flex-wrap justify-center">
+    
+    <div className="bg-yellow-500 w-40 h-40 text-center flex items-center justify-center">luca</div>
+    <div className={stilecard}>luca</div>
+    <div className={stilecard}>male</div>
+  
+    </div>
+                                              
 };
