e40db4475a1c0bea598c9a67d2b2518daa9ffb0c
LUCA <lucadoniselli07@gmail.com>
2026-01-15 13:26:54 +0000

    VERAVERIFICA

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,29 +1,37 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
+import pokemonImg from "./pokemon.jpeg"; 
 
-
-const stilecard = "bg-purple-500 w-64 h-80 text-center flex flex-col items-center justify-center rounded-xl p-4";
-import pokemonImg from "./pokemon.jpeg"; // CORRETT
 export const Root = () => {
   return (
-    <div className="text-xl space-x-7 flex flex-wrap justify-center gap-6 p-10">
+    <div className="text-xl flex flex-wrap justify-center gap-6 p-10">
       
      
-      <div className="bg-yellow-500 w-64 h-80 text-center flex flex-col items-center justify-center rounded-xl shadow-lg p-4">
+      <div className="relative bg-yellow-500 w-64 h-80 text-center flex flex-col items-center justify-center rounded-xl shadow-lg p-4">
         
-        
+      
+        <div className="absolute top-4 left-6 font-bold text-gray-800 text-2xl">
+          0
+        </div>
+
         <img 
           src={pokemonImg} 
           alt="Froakie" 
           className="w-40 h-40 object-contain mb-4" 
         />
         
+    
         <span className="font-bold uppercase tracking-widest text-2xl">
           FROAKIE
         </span>
+
+    
+        <div className="absolute bottom-4 right-4 bg-blue-500 text-white px-3 py-1 rounded-lg text-sm font-bold uppercase shadow-sm">
+          Acqua
+        </div>
         
       </div>
-          
+
     </div>
   );
 };
